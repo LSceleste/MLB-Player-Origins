36f88301040385ea19a7b8ce083c761f5a75eb87
Peter Hardy <peterphardy@gmail.com>
2019-06-29 11:41:02 -0700

    stylizing

--- app.py
+++ app.py
@@ -40,8 +40,6 @@ class Baseball(db.Model):
 # Create database tables
 @app.before_first_request
 def setup():
-    # Recreate database each time for demo
-    # db.drop_all()
     db.create_all()
 
 
@@ -103,12 +101,8 @@ def stats_data2():
     flags = [result[8] for result in results2]
     deathCity = [result[9] for result in results2]
     deathCountry = [result[10] for result in results2]
+    
     print(results2)
-    # # Generate the plot trace
-    # trace = {
-    #     "x": nameLast,
-    #     "y": birthYear,
-    #     "type": "bar"
     
     return jsonify(results2)
 
@@ -116,141 +110,7 @@ def stats_data2():
 def sources():
     """Render Home Page."""
     return render_template("sources.html")
-# @app.route("/emoji_id")
-# def emoji_id_data():
-#     """Return emoji score and emoji id"""
-
-#     # Query for the emoji data using pandas
-#     query_statement = db.session.query(Emoji).\
-#         order_by(Emoji.score.desc()).\
-#         limit(10).statement
-#     df = pd.read_sql_query(query_statement, db.session.bind)
-
-#     # Format the data for Plotly
-#     trace = {
-#         "x": df["emoji_id"].values.tolist(),
-#         "y": df["score"].values.tolist(),
-#         "type": "bar"
-#     }
-#     return jsonify(trace)
-
-
-# @app.route("/emoji_name")
-# def emoji_name_data():
-#     """Return emoji score and emoji name"""
-
-#     # Query for the top 10 emoji data
-#     results = db.session.query(Emoji.name, Emoji.score).\
-#         order_by(Emoji.score.desc()).\
-#         limit(10).all()
-#     df = pd.DataFrame(results, columns=['name', 'score'])
-
-#     # Format the data for Plotly
-#     plot_trace = {
-#         "x": df["name"].values.tolist(),
-#         "y": df["score"].values.tolist(),
-#         "type": "bar"
-#     }
-#     return jsonify(plot_trace)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import numpy as np
-
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-
-# from flask import Flask, jsonify
-
-
-# #################################################
-# # Database Setup
-# #################################################
-# engine = create_engine("sqlite:///baseball.sqlite")
-
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
-
-# # Save reference to the table
-# BaseballPlayers = Base.classes.baseball
-
-# # Create our session (link) from Python to the DB
-# session = Session(engine)
-
-# #################################################
-# # Flask Setup
-# #################################################
-# app = Flask(__name__)
-
-
-# #################################################
-# # Flask Routes
-# #################################################
-
-# @app.route("/")
-# def welcome():
-#     """List all available api routes."""
-#     return (
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/birthcity<br/>"
-#         f"/api/v1.0/latlng"
-#     )
-
-# @app.route("/api/v1.0/birthcity")
-# def lats():
-#     """Return a list of all birthcity"""
-#     # Query all lats
-#     results = session.query(BaseballPlayers.birthCity).all()
-
-#     # Convert list of tuples into normal list
-#     all_birthCity = list(np.ravel(results))
-
-#     return jsonify(all_birthCity)
-
-# @app.route("/api/v1.0/latlng")
-# def latlng():
-#     """Return a list of playerstats including latitude, longitude"""
-#     # Query all latlngs
-#     results = session.query(BaseballPlayers.latitude, BaseballPlayers.longitude).all()
-
-#     # Create a dictionary from the row data and append to a list of all_latlngs
-#     all_latlng = []
-#     for latitude, longitude in results:
-#         stats_dict = {}
-#         stats_dict["latitude"] = latitude
-#         stats_dict["longitude"] = longitude
-#         all_latlng.append(stats_dict)
-
-#     return jsonify(all_latlng)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
-
-
